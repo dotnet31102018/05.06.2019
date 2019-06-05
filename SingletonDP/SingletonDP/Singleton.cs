1dbb50c9e6e56fa67d3c5b556ea257ac7429bca4
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-06-05 19:43:57 +0300

    Add files via upload

---
+++ SingletonDP/SingletonDP/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDP
+{
+    class Singleton
+    {
+
+        private static Singleton _instance;
+        private static object key = new object();
+        private Singleton()
+        {
+
+        }
+
+        public void WriteLog(string log)
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (key)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
