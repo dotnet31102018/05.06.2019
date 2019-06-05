829a7b2c3c06e3627f91537b7a75095f438217a2
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-06-05 18:28:26 +0300

    Add files via upload

---
+++ 0506HW/0506/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _0506
+{
+    class Program
+    {
+        private static void PrintMe()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void PrintHelloWorld()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Hello world!");
+                Thread.Sleep(250);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadExecutor executor = new ThreadExecutor();
+            for (int i = 0; i < 20; i++)
+            {
+                executor.Add(new Thread(PrintMe));
+                executor.Execute();
+                executor.Add(new Thread(PrintHelloWorld));
+                executor.Add(new Thread(() => Console.WriteLine("Hello from lambda!!") ) );
+                executor.Execute();
+            }
+            Console.WriteLine("Main has finished");
+        }
+    }
+}
---
+++ 0506HW/0506/ThreadExecutor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _0506
+{
+    class ThreadExecutor
+    {
+        Queue<Thread> _threadsQueue = new Queue<Thread>();
+
+        public void Add(Thread thread)
+        {
+            lock (this)
+            {
+                Console.WriteLine($"new thread entered the queue");
+                if (thread != null)
+                {
+                    _threadsQueue.Enqueue(thread);
+                }
+            }
+        }
+
+        public void Execute()
+        {
+            lock (this)
+            {
+                while (_threadsQueue.Count > 0)
+                {
+                    // with Peek
+                    //Thread _current = _threadsQueue.Peek();
+                    //_current.Start();
+                    //_current.Join();
+                    //_threadsQueue.Dequeue();
+
+                    Thread current = _threadsQueue.Dequeue();
+
+                    current.Start();
+
+                    //current.Join();
+                }
+            }
+        }
+    }
+}
